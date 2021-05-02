92019cad4cf5ffdc86d9110563b74664f802f707
Michael Morris <angelicstrings@gmail.com>
2021-05-02 16:33:21 -0500

    Notated the minor change to the code.

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -8,6 +8,9 @@ namespace ASPNet_Core_Web_API_Project_050121
 
         public int TemperatureC { get; set; }
 
+        // Below is a stylistic change which doesn't affect the results of this particular program...
+        // But mathematically *(1.8), *(9/5), /(5/9) would all have been 'better' than /.5556....
+        
         public int TemperatureF => 32 + (int)(TemperatureC * 1.8);
 
         public string Summary { get; set; }
